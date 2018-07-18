82841bdd140608419f3f8a6d9773a2109c698ccf
Kathryn Lewis <rynrie@gmail.com>
2018-07-18 16:10:36 -0700

    Fill in base home_page test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
+
 from lists.views import home_page
 
 
@@ -9,3 +11,12 @@ class SmokeTest(TestCase):
     # django uses resolve() to map a URL to the corresponding view function
     found = resolve('/')
     self.assertEqual(found.func, home_page)
+
+
+  def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode('utf8')
+    self.assertTrue(html.startswith('<html>'))
+    self.assertIn('<title>To-Do lists</title>', html)
+    self.assertTrue(html.endswith('</html>'))
